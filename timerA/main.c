40ac2550ca325cf4cff340a8376dd785ad78abfd
coprea <claudiuo@gmail.com>
2013-02-06 16:39:20 -0700

    reformat comments; divide clock by 2 for slower timer

--- timerA/main.c
+++ timerA/main.c
@@ -8,69 +8,50 @@ void toggleLeds(int,int);
 
 int main(void)
 {
-	// init watchdog timer to off
-	WDTCTL = WDTPW + WDTHOLD;
+	WDTCTL = WDTPW + WDTHOLD; // init watchdog timer to off
 
-	// set LED output to P1.0 and P1.1
-	P1DIR |= 0x03;
+	P1DIR |= 0x03; // set LED output to P1.0 and P1.1
 	// similar: P1DIR |= BIT0 + BIT1;
 
-	// turn on LEDs
-	P1OUT |= BIT0 + BIT1;
+	P1OUT |= BIT0 + BIT1; // turn on LEDs
 
-	// set P1.2 as input
-	P1DIR &= ~0x04;
-	// enable resistor on P1.2
-    P1REN |=  0x04;
-    // enable interrupts on P1.2
-    P1IE  |=  0x04;
+	P1DIR &= ~0x04; // set P1.2 as input
+	P1REN |=  0x04; // enable resistor on P1.2
+    P1IE  |=  0x04; // enable interrupts on P1.2
 
 	// set up Timer A
-	// ACLK on sourced to the VLO
-	BCSCTL3 |= LFXT1S_2;
-	// enable interrupts for Timer A
-	TACCTL0 = CCIE;
-	// start the timer: count up to this value: approx. 4s (VLO around 12kHz)
-//	TACCR0 = 48000;
-	// Timer A count up (MC_1) using ACLK (TASSEL_1)
-	TACTL = MC_1+TASSEL_1;
-	// make sure timer doesn't run
-	TACCR0 = 0;
+	BCSCTL3 |= LFXT1S_2;        // ACLK on sourced to the VLO
+	TACCTL0 = CCIE;             // enable interrupts for Timer A
+//	TACCR0 = 48000;             // start the timer: approx. 4s (VLO around 12kHz) * 2 (ID_1) = 8s
+	TACCR0 = 0;                 // make sure timer is stopped
+	TACTL = MC_1+TASSEL_1+ID_1; // Timer A count up (MC_1) using ACLK (TASSEL_1) divided by 2 (ID_1)
 
-	// enable interrupts and sleep
-    //__bis_SR_register( GIE + LPM4_bits );
-
-   	// enable interrupts and sleep (LPM3 leaves only ACLK running)
-    __bis_SR_register( GIE + LPM3_bits );
+    __bis_SR_register( GIE + LPM3_bits ); // enable interrupts and sleep
+    									  // (LPM3 leaves only ACLK running)
 }
 
-// button ISR
-#pragma vector=PORT1_VECTOR
+#pragma vector=PORT1_VECTOR    // button ISR
 interrupt void Port_1 ( void )
 {
-	// clear interrupt flag
-	P1IFG &= ~0x04;
-	toggleLeds(5,FAST);
+	P1IFG &= ~0x04;     // clear interrupt flag
 
-	// turn on LEDs
-	P1OUT |= 0x03;
+	TACCR0 = 0;         // stop the timer
 
-	// stop and restart the timer
-	TACCR0 = 0;
-	TACCR0 = 48000;
+	toggleLeds(5,FAST); // blink the LEDs fast to signal button ISR
+
+	P1OUT |= 0x03;      // turn on LEDs
+
+	TACCR0 = 48000;     // restart the timer
 }
 
-// Timer A ISR
-#pragma vector=TIMERA0_VECTOR
+#pragma vector=TIMERA0_VECTOR    // Timer A ISR
 interrupt void Timer_A ( void )
 {
-	// blink the LEDs to signal timer ISR
-	toggleLeds(5,SLOW);
- 	// turn off LEDs
- 	P1OUT &= ~0x03;
+	TACCR0 = 0;         // stop the timer
 
-	// stop the timer
-	TACCR0 = 0;
+	toggleLeds(5,SLOW); // blink the LEDs slow to signal timer ISR
+
+ 	P1OUT &= ~0x03;     // turn off LEDs
 }
 
 void toggleLeds (int howMany, int timeout)
@@ -78,13 +59,12 @@ void toggleLeds (int howMany, int timeout)
 	int i,j = 0;
     for ( j=0; j<howMany; j++) {
 
-		// toggle LEDs
-		P1OUT ^= 0x03;
+		P1OUT ^= 0x03;  // toggle LEDs
 		for ( i=0; i<timeout; i++) {
 			__no_operation();
 	 	}
-	 	// turn off one LED
-	 	P1OUT &= ~0x01;
+
+	 	P1OUT &= ~0x01; // turn off one LED
 		for ( i=0; i<timeout; i++) {
 			__no_operation();
 	 	}
