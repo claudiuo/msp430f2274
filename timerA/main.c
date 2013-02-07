97acf5868a9460372fe30e8940e58328ddb35960
claudiu <claudiuo@gmail.com>
2013-02-06 22:51:44 -0700

    working code: each button push wakes up the CPU and (re)starts the timer; after timer counts up, irq fires, LEDs blink, timer is stopped and cpu goes to sleep

--- timerA/main.c
+++ timerA/main.c
@@ -4,7 +4,7 @@
 const unsigned int FAST = 5000;
 const unsigned int SLOW = 15000;
 
-void toggleLeds(int,int);
+void toggleLeds(int,int,unsigned int);
 
 int main(void)
 {
@@ -22,9 +22,6 @@ int main(void)
 	// set up Timer A
 	BCSCTL3 |= LFXT1S_2;        // ACLK on sourced to the VLO
 	TACCTL0 = CCIE;             // enable interrupts for Timer A
-//	TACCR0 = 48000;             // start the timer: approx. 4s (VLO around 12kHz) * 2 (ID_1) = 8s
-	TACCR0 = 0;                 // make sure timer is stopped
-	TACTL = MC_1+TASSEL_1+ID_1; // Timer A count up (MC_1) using ACLK (TASSEL_1) divided by 2 (ID_1)
 
     __bis_SR_register( GIE + LPM3_bits ); // enable interrupts and sleep
     									  // (LPM3 leaves only ACLK running)
@@ -33,38 +30,39 @@ int main(void)
 #pragma vector=PORT1_VECTOR    // button ISR
 interrupt void Port_1 ( void )
 {
-	P1IFG &= ~0x04;     // clear interrupt flag
-
-	TACCR0 = 0;         // stop the timer
-
-	toggleLeds(5,FAST); // blink the LEDs fast to signal button ISR
-
+	toggleLeds(5,FAST,BIT0+BIT1); // blink both LEDs fast to signal button ISR
 	P1OUT |= 0x03;      // turn on LEDs
 
-	TACCR0 = 48000;     // restart the timer
+	// restart the timer
+	TAR = 0;                    // reset the counter
+	TACCR0 = 48000;             // set the count up value: approx. 4s
+								// (VLO around 12kHz) * 2 (ID_1) = 8s
+	TACTL = MC_1+TASSEL_1+ID_1; // Timer A count up (MC_1) using
+								// ACLK (TASSEL_1) divided by 2 (ID_1)
+	P1IFG &= ~0x04;     // clear interrupt flag
 }
 
 #pragma vector=TIMERA0_VECTOR    // Timer A ISR
 interrupt void Timer_A ( void )
 {
-	TACCR0 = 0;         // stop the timer
-
-	toggleLeds(5,SLOW); // blink the LEDs slow to signal timer ISR
+	TACTL = MC_0;       // timer halted
 
  	P1OUT &= ~0x03;     // turn off LEDs
+	toggleLeds(10,SLOW,BIT0); // blink 	red LED slow
+    P1OUT &= ~0x03;     // turn off LEDs
 }
 
-void toggleLeds (int howMany, int timeout)
+void toggleLeds (int howMany, int timeout, unsigned int leds )
 {
 	int i,j = 0;
     for ( j=0; j<howMany; j++) {
 
-		P1OUT ^= 0x03;  // toggle LEDs
+		P1OUT ^= leds;  // toggle LEDs
 		for ( i=0; i<timeout; i++) {
 			__no_operation();
 	 	}
 
-	 	P1OUT &= ~0x01; // turn off one LED
+	 	P1OUT &= ~leds; // turn off one LED
 		for ( i=0; i<timeout; i++) {
 			__no_operation();
 	 	}
