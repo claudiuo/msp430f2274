4c7446abf7748d362df8009c98c4745433e5d547
coprea <claudiuo@gmail.com>
2013-02-08 15:59:14 -0700

    use var to reset/stop timer set P2.3 for external interrupt and added ISR for it

--- timerA/main.c
+++ timerA/main.c
@@ -1,10 +1,13 @@
 #include "msp430.h"
 #include "in430.h"
 
-const unsigned int FAST = 5000;
+const unsigned int FAST = 10000;
 const unsigned int SLOW = 15000;
 
+static unsigned int timerCount = 0; // use it to count timer overflows
+
 void toggleLeds(int,int,unsigned int);
+void doIsrWork(int);
 
 int main(void)
 {
@@ -19,9 +22,18 @@ int main(void)
     P1REN |=  0x04; // enable resistor on P1.2
     P1IE  |=  0x04; // enable interrupts on P1.2
 
+    // set P2 port
+    P2DIR &= ~BIT3; // set P2.3 as input
+    P2OUT &= ~BIT3; // pin is pulled down
+    P2REN |=  BIT3; // enable resistor on P2.3
+    P2IES &= ~BIT3; // low to high transition
+    P2IE  |=  BIT3; // enable interrupts on P2.3
+
     // set up Timer A
     BCSCTL3 |= LFXT1S_2;        // ACLK on sourced to the VLO
     TACCTL0 = CCIE;             // enable interrupts for Timer A
+    TACCR0 = 24000;             // set the count up value: approx. 1s
+                                // (VLO around 12kHz) * 1 (ID_0) = 1s
 
     __bis_SR_register( GIE + LPM3_bits ); // enable interrupts and sleep
                                           // (LPM3 leaves only ACLK running)
@@ -30,26 +42,43 @@ int main(void)
 #pragma vector=PORT1_VECTOR    // button ISR
 interrupt void Port_1 ( void )
 {
-    toggleLeds(5,FAST,BIT0+BIT1); // blink both LEDs fast to signal button ISR
-    P1OUT |= 0x03;  // turn on LEDs
+	doIsrWork(BIT0);
+    P1IFG &= ~0x04;               // clear interrupt flag
+}
 
-    // restart the timer
-    TAR = 0;                    // reset the counter
-    TACCR0 = 48000;             // set the count up value: approx. 4s
-                                // (VLO around 12kHz) * 2 (ID_1) = 8s
-    TACTL = MC_1+TASSEL_1+ID_1; // Timer A count up (MC_1) using
-                                // ACLK (TASSEL_1) divided by 2 (ID_1)
-    P1IFG &= ~0x04; // clear interrupt flag
+#pragma vector=PORT2_VECTOR    // P2 ISR
+interrupt void Port_2 ( void )
+{
+	doIsrWork(BIT1);
+    P2IFG &= ~BIT3;               // clear interrupt flag
 }
 
 #pragma vector=TIMERA0_VECTOR    // Timer A ISR
 interrupt void Timer_A ( void )
 {
-    TACTL = MC_0;       // timer halted
+    P1OUT &= ~0x03;                // turn off LEDs
+    toggleLeds(10,SLOW,BIT0+BIT1); // blink both LEDs slow to signal timer ISR
+    P1OUT &= ~0x03;                // turn off LEDs
 
-    P1OUT &= ~0x03;     // turn off LEDs
-    toggleLeds(10,SLOW,BIT0); // blink 	red LED slow
-    P1OUT &= ~0x03;     // turn off LEDs
+    switch(TAIV) {
+        case TAIV_NONE:
+            if(timerCount++ == 9) { // this happens after 10s
+                TACTL = MC_0;       // timer halted
+            }
+            break;
+    }
+}
+
+void doIsrWork (int led)
+{
+    P1OUT &= ~0x03;               // turn off LEDs
+    toggleLeds(10,SLOW,led);      // blink red/green LED to signal P1/P2 ISR
+    P1OUT &= ~led;     		      // turn off LED that was blinking
+
+    timerCount = 0;               // reset the timerCount
+    // (re)start the timer
+    TACTL = MC_1+TASSEL_1+ID_0;   // Timer A count up (MC_1) using
+                                  // ACLK (TASSEL_1) no division (ID_0)
 }
 
 void toggleLeds (int howMany, int timeout, unsigned int leds )
